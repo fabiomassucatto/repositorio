02317e7f1e0dd0b4f9395be86f8b35a82c006487
fabiomassucatto <fabiomassucatto0102@gmail.com>
2025-08-30 00:04:35 -0300

    novas

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida ('Suco de Laranja', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho= Prato('paozinho', 2.0, 'paozinho com manteiga')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_item_cardapio(bebida_suco)
 restaurante_praca.adicionar_item_cardapio(prato_paozinho)
 
